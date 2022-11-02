885de2587ae6c887ab847b62d56ca2c013db0f9e
GiulianoLievore <giul.lievore@gmail.com>
2022-11-01 23:56:01 -0300

    aplicando js

---
+++ js/miarchivo.js
@@ -0,0 +1,37 @@
+class Cliente {
+    constructor(nombre, genero) {
+    this.nombre = nombre;
+    this.genero = genero.toLowerCase();
+    }
+}
+let productos = [{
+    producto: "pantalon",
+    precio: 100000
+    },
+    {
+    producto: "remeras",
+    precio: 2000
+    }
+]
+  const calcularIva = a => a * 0.21;
+
+  //interaccion con el cliente
+
+let cliente1 = new Cliente(prompt("Cual es tu nombre?"), prompt("Sos mujer u hombre?"))
+if (cliente1.genero === "mujer") {
+    console.log("Bienvenida " + cliente1.nombre + " " + "te mostraremos prendas de mujer")
+} else {
+    console.log("Bienvenido " + cliente1.nombre + " " + "te mostraremos prendas de hombre")
+}
+
+
+let comprar = prompt("¿Querés comprar remeras o pantalones?").toLowerCase()
+if (comprar === "remeras" || comprar === "pantalon") {
+    let cantidad = parseInt(prompt(`¿Cúantas ${comprar} querés?`))
+    let subtotal = cantidad * productos.find((product) => product.producto === comprar).precio;
+    let total = subtotal + calcularIva(subtotal);
+    console.log(total);
+
+} else {
+    alert("Elige solo entre remeras y pantalon")
+}
