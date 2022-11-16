cbe79aa103235dda151cb97a20b2a1f6a74352d9
GiulianoLievore <giul.lievore@gmail.com>
2022-11-15 23:34:19 -0300

    segunda preentrega

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -4,6 +4,7 @@ class Cliente {
     this.genero = genero.toLowerCase();
     }
 }
+
 let productos = [{
     producto: "pantalon",
     precio: 100000
@@ -13,25 +14,34 @@ let productos = [{
     precio: 2000
     }
 ]
-  const calcularIva = a => a * 0.21;
 
-  //interaccion con el cliente
+const calcularIva = a => a * 0.21;
 
-let cliente1 = new Cliente(prompt("Cual es tu nombre?"), prompt("Sos mujer u hombre?"))
+//interaccion con el cliente
+
+let cliente1 = new Cliente(prompt("Cual es tu nombre?"), prompt("Sos mujer u hombre?"));
+
 if (cliente1.genero === "mujer") {
-    console.log("Bienvenida " + cliente1.nombre + " " + "te mostraremos prendas de mujer")
+    console.log("Bienvenida " + cliente1.nombre + " " + "te mostraremos prendas de mujer");
 } else {
-    console.log("Bienvenido " + cliente1.nombre + " " + "te mostraremos prendas de hombre")
+    console.log("Bienvenido " + cliente1.nombre + " " + "te mostraremos prendas de hombre");
 }
 
+let comprar = prompt("¿Querés comprar remeras o pantalones?").toLowerCase();
 
-let comprar = prompt("¿Querés comprar remeras o pantalones?").toLowerCase()
 if (comprar === "remeras" || comprar === "pantalon") {
-    let cantidad = parseInt(prompt(`¿Cúantas ${comprar} querés?`))
+    let cantidad = parseInt(prompt(`¿Cúantas ${comprar} querés?`));
     let subtotal = cantidad * productos.find((product) => product.producto === comprar).precio;
     let total = subtotal + calcularIva(subtotal);
-    console.log(total);
-
+    alert(total);
 } else {
-    alert("Elige solo entre remeras y pantalon")
+    alert("Elige solo entre remeras y pantalon");
+}
+
+let info = prompt("¿Querés mas info?").toLowerCase();
+
+while(info === "si"){
+    alert("Envianos un mensaje para descubrir mas sobre nuestra empresa");
+
+    info = prompt("¿Necesitas algo mas?").toLowerCase();
 }
