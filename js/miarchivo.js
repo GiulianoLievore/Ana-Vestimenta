f150bd856ffc028faa1d3e91ab5fc139eefe4522
GiulianoLievore <giul.lievore@gmail.com>
2022-11-29 19:28:58 -0300

    tercera preentrega

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,10 +1,47 @@
-class Cliente {
-    constructor(nombre, genero) {
-    this.nombre = nombre;
-    this.genero = genero.toLowerCase();
-    }
-}
+// Reconocimiento de usuario
+const nomUsuario = document.querySelector("#nombreUsuario");
+const localUsuario = localStorage.getItem("Usuario");
+const submit = document.querySelector("#submit");
+const usuario = document.querySelector("#usuario");
 
+localUsuario === null ? 
+Swal.fire(
+    'Recuerda poner tu nombre de usuario',
+    '',
+    'info'
+)
+
+// interaccion con el usuario donde se le pide ingresar su nombre y posteriormente guardarlo
+: 
+nomUsuario.innerText = "Bienvenido " + localUsuario + " a la página oficial de ventas de Ana vestimenta";
+nomUsuario.classList = "text-center";
+
+submit.addEventListener("click", () =>{
+    const value = usuario.value;
+
+    value === "" ? 
+    (Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Escribiste un caracter vacio'
+    }), submit.value = "Reintenta escribir tu nombre")
+    :
+    Swal.fire({
+        title: '¿Estas seguro de que quieres guardar este nombre?',
+        icon: 'info',
+        showCancelButton: true,
+        confirmButtonText: 'Guardar',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          location.reload(); 
+        }
+      })  
+
+    localStorage.setItem("Usuario", value);
+})
+
+
+// Productos hechos en objetos
 let productos = [{
     producto: "pantalon",
     precio: 100000
@@ -12,36 +49,69 @@ let productos = [{
     {
     producto: "remeras",
     precio: 2000
-    }
+    },
+    {
+    producto: "zapatilla",
+    precio: 2500
+    },
+    {
+    producto: "Sombrero",
+    precio: 1500
+    },
+    {
+    producto: "Pollera",
+    precio: 3000
+    },
+    {
+    producto: "Jean azul",
+    precio: 2000
+    },
+    {
+    producto: "Camisa leñadora",
+    precio: 4000
+    },
+    {
+    producto: "Buzo Rojo",
+    precio: 5000
+    },     
 ]
 
-const calcularIva = a => a * 0.21;
+// const calcularIva = a => a * 0.21;
 
-//interaccion con el cliente
+const productoInfo = document.querySelector('#productoInfo');
+const precios = [];
 
-let cliente1 = new Cliente(prompt("Cual es tu nombre?"), prompt("Sos mujer u hombre?"));
+// Creacion y renderizacion de Producto
+for (const prenda of productos){
+    const {producto, precio} = prenda;
 
-if (cliente1.genero === "mujer") {
-    console.log("Bienvenida " + cliente1.nombre + " " + "te mostraremos prendas de mujer");
-} else {
-    console.log("Bienvenido " + cliente1.nombre + " " + "te mostraremos prendas de hombre");
+    const div = document.createElement('div');
+    div.className = 'card m-3'
+    div.style = 'width: 18rem;'
+    div.innerHTML = `
+                <div class="card-body" >
+                  <h5 class="card-title text-capitalize text-center text-danger">${producto}</h5>
+                  <p class="card-text fs-5">Prendas especialmente hecha para vos con un unico precio de tan solo $${precio}.</p>
+                </div>
+    `
+
+    precios.push(precio);
+
+    productoInfo.append(div);
 }
 
-let comprar = prompt("¿Querés comprar remeras o pantalones?").toLowerCase();
+// Renderizacion de parrafos que muestran el producto mas caro y mas barato
+const productoMaxMin = document.querySelector('#productoMaxMin');
 
-if (comprar === "remeras" || comprar === "pantalon") {
-    let cantidad = parseInt(prompt(`¿Cúantas ${comprar} querés?`));
-    let subtotal = cantidad * productos.find((product) => product.producto === comprar).precio;
-    let total = subtotal + calcularIva(subtotal);
-    alert(total);
-} else {
-    alert("Elige solo entre remeras y pantalon");
-}
+const parrafo = document.createElement("p");
+parrafo.className = "text-center fs-4 border border-secondary";
+parrafo.innerText = "El producto mas caro de nuestras prendas tiene un valor de: $" + Math.max(...precios); 
 
-let info = prompt("¿Querés mas info?").toLowerCase();
+productoMaxMin.append(parrafo);
 
-while(info === "si"){
-    alert("Envianos un mensaje para descubrir mas sobre nuestra empresa");
+const parrafoDos = document.createElement("p");
+parrafoDos.className = "text-center fs-4 border border-secondary";
+parrafoDos.innerText = "El producto mas barato de nuestras prendas tiene un valor de: $" + Math.min(...precios); 
 
-    info = prompt("¿Necesitas algo mas?").toLowerCase();
-}
+productoMaxMin.append(parrafoDos);
+
